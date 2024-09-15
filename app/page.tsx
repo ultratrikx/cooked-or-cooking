c62f051ab2bc6e38645ef52176f24a8db8f04dd2
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2024-09-14 22:30:20 -0400

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        rohanth marem
+      </h1>
+      <p className="mb-4">
+        {`are you cooked or are you cooking?? track your productivity and find out the results at the end of the event!! download the app today at:  `}<a href="mailto:rohanthm@outlook.com">rohanthm@outlook.com.</a>
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
