50a97fb918cb0aa16705876aa1490462d7f7537d
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2024-09-14 22:50:00 -0400

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -16,5 +16,50 @@ function ArrowIcon() {
 }
 
 export default function Footer() {
-  return ()
+  return (
+    <footer className="mb-16">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/rss"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">rss</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/are-we-cooked-"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">github</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/ultratrikx/are-we-cooked-"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">view source</p>
+          </a>
+        </li>
+        </ul>
+        <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+          © {new Date().getFullYear()} MIT Licensed
+        </p>
+        {/* <form id="newsletter-form">
+          <input type="email" id="email" placeholder="Your email" required></input>
+          <button type="submit">Subscribe</button>
+        </form> */}
+        </footer>
+)
 }
