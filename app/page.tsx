5fd9ce7847f186a9140ca02497870fef62308d67
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2024-09-14 22:45:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ export default function Page() {
       <p className="mb-4">
         {`are you cooked or are you cooking?? track your productivity and find out the results at the end of the event!! download the app today at:  `}<a href="mailto:rohanthm@outlook.com">rohanthm@outlook.com.</a>
       </p>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        https://github.com/ultratrikx/are-you-cooked-/releases/tag/fr_release
+      </h1>
       <div className="my-8">
         <BlogPosts />
       </div>
