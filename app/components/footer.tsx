fbf33afbc714ccc4096d19477cf14816a742cdec
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2024-09-14 22:56:36 -0400

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/cooked-or-cooking"
+            href="https://github.com/ultratrikx/cooked-or-cooking"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
