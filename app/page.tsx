b9b988d7e17f8b3fdcc1de08d95f1cae35cf3661
Rohanth Marem <64563315+ultratrikx@users.noreply.github.com>
2024-09-14 22:53:53 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { BlogPosts } from 'app/components/posts'
+import React from 'react';
+import { BlogPosts } from 'app/components/posts';
 
 export default function Page() {
   return (
@@ -7,11 +8,16 @@ export default function Page() {
         cooked or cooking? htn 24
       </h1>
       <p className="mb-4">
-        {`are you cooked or are you cooking?? track your productivity and find out the results at the end of the event!! download the app today at:  `}
+        {`are you cooked or are you cooking?? track your productivity and find out the results at the end of the event!! download the app today:`}
       </p>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">\
-        https://github.com/ultratrikx/are-you-cooked-/releases/tag/fr_release
-      </h1>
+      <a 
+        href="https://github.com/ultratrikx/are-you-cooked-/releases/tag/fr_release"
+        className="inline-block px-4 py-2 mb-8 text-white bg-blue-500 rounded hover:bg-blue-600 transition-colors"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Download App
+      </a>
       <div className="my-8">
         <BlogPosts />
       </div>
